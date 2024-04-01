a72be8ca99f88566e849d00d3f22324ae316c894
9rato <moshood.ajose6@gmail.com>
2024-04-01 11:35:13 +0000

    update db

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 
 /* Database credentials */
-define('DB_SERVER', 'localhost:3306');
-define('DB_USERNAME', 'multilinksbase_shippings');
-define('DB_PASSWORD', 'shipping123@');
-define('DB_NAME', 'multilinksbase_frightcargo');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'cargojij_shipping');
+define('DB_PASSWORD', 'cargojij_shipping');
+define('DB_NAME', 'cargojij_shipping');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
