9ce5d5b28869811eb887f94733d5038e106b8f5e
9rato <moshood.ajose6@gmail.com>
2024-04-03 00:23:23 +0000

    WET

--- admin/pages/insertwallet.php
+++ admin/pages/insertwallet.php
@@ -3,9 +3,9 @@
 
 // Database credentials
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'frightcargo');
+define('DB_USERNAME', 'cargojij_shipping');
+define('DB_PASSWORD', 'cargojij_shipping');
+define('DB_NAME', 'cargojij_shipping');
 
 // Attempt to connect to MySQL database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
